bec06047e58e3dec4faf4ae5b8b93f12cc547b98
Leandro Solidum <leo.g.solidum@gmail.com>
2025-01-09 00:18:30 -0800

    add some code templates

---
+++ templates/dfs.js
@@ -0,0 +1,32 @@
+// Binary tree: DFS (recursive)
+let dfsRecursive = root => {
+    if (!root) {
+        return;
+    }
+
+    let ans = 0;
+
+    // do logic
+    dfs(root.left);
+    dfs(root.right);
+    return ans;
+}
+
+// binary tree: DFS (iterative)
+let dfsIterative = root => {
+    let stack = [root];
+    let ans = 0;
+
+    while (stack.length) {
+        let node = stack.pop();
+        // do logic
+        if (node.left) {
+            stack.push(node.left);
+        }
+        if (node.right) {
+            stack.push(node.right);
+        }
+    }
+
+    return ans;
+}
